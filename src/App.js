075bf2f1ef46c139e332f77164ae800a9ca00816
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-16 21:35:40 +0100

    Currently adding in a const to get advice from the API. Added in .catch function to catch any errors

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ class App extends React.Component {
   state = { advice: '' };
 
   componentDidMount() {
-    console.log('COMPONENT DID MOUNT')
-    axios.get();
+    this.fetchAdvice();
   }
 
   fetchAdvice = () => {
-    axios.get();
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip
+        console.log(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
